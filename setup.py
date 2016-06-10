c66e733707da9d2fceba7d523e2823ccec138769
Rashi Agrawal <rashiaa@sisterquota-lm.corp.yahoo.com>
2016-06-10 13:37:09 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/rashi283/funniest',
+      author='Rashi Agrawal',
+      author_email='rashi283@yahoo.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
